d1be00aca587fc0e1abc79bd43ec0531a92ca27c
Ahiwamu <niceziio08@gmail.com>
2025-09-08 11:40:14 +0700

    test

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,20 +1,43 @@
-import { Card, Group, Badge, ActionIcon, Text } from "@mantine/core";
-import { IconTrash } from "@tabler/icons-react";
+// import { Card, Group, Badge, ActionIcon, Text } from "@mantine/core";
+// import { IconTrash } from "@tabler/icons-react";
 
-type FoodProps = {
-  name: string;
-  price: number | string;
-  quantity: number | string;
-  category: string;
-  onDelete: () => void;
-};
+// type FoodProps = {
+//   name: string;
+//   price: number | string;
+//   quantity: number | string;
+//   category: string;
+//   onDelete: () => void;
+// };
 
-export default function ItemCard() {
-  // หากต้องการเปลี่ยนแปลง type ชนิด string เป็น number สามารถใช้วิธีการดังโค้ดตัวอย่างด้านล่างนี้ได้
-  let val_number: number = Number("500.0");
-  console.log(val_number + 100); // 600.0
+// export default function ItemCard() {
+//   // หากต้องการเปลี่ยนแปลง type ชนิด string เป็น number สามารถใช้วิธีการดังโค้ดตัวอย่างด้านล่างนี้ได้
+//   let val_number: number = Number("500.0");
+//   console.log(val_number + 100); // 600.0
 
-  return {
-    /* Type additional text here. */
-  };
-}
+//   return (
+//     <Stack w="100%">
+//         {items.map((task) => (
+//           <Group justify="space-between" align="flex-start">
+//             <Stack>
+//               <Text>{task.name}</Text>
+
+//               <Text size="sm" c="dimmed">
+//                 {task.price} + Bath x {task.quantity} = {Number(task.price) * Number(task.quantity)} Bath
+//               </Text>
+
+//               <ActionIcon
+//                     variant="filled"
+//                     color="red"
+//                     aria-label="Settings"
+//                     onClick={() => deleteTask(task.id)}
+//                   >
+//                     <IconTrash stroke={1.5} />
+//                   </ActionIcon>
+
+//             </Stack>
+//           </Group>
+//         ))}
+//       </Stack>
+
+//   );
+// }
--- src/pages/TrackerPage.tsx
+++ src/pages/TrackerPage.tsx
@@ -1,6 +1,17 @@
-import { useState } from "react";
-import { Button, Stack, Title, Divider, Container } from "@mantine/core";
-import { v4 as uuidv4 } from "uuid";
+import {
+  // Button,
+  Stack,
+  Title,
+  Divider,
+  Container,
+  Text,
+  // Card,
+  // Group,
+  // ActionIcon,
+} from "@mantine/core";
+//import { v4 as uuidv4 } from "uuid";
+import { useEffect, useState } from "react";
+// import AddTaskModal from "../components/Modal";
 
 type FoodItem = {
   id: string;
@@ -11,27 +22,64 @@ type FoodItem = {
 };
 
 export default function FoodTracker() {
-  const [opened, setOpened] = useState(false);
   const [items, setItems] = useState<FoodItem[]>([]);
-  const categories = ["Main Course", "Drink", "Dessert"];
 
+  // const [opened, setOpened] = useState(false);
+  useEffect(() => {
+    const stored = localStorage.getItem("my-items");
+    if (stored) {
+      const parsed = JSON.parse(stored);
+      setItems(parsed);
+    }
+    setItems(items);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("my-items", JSON.stringify(items));
+  }, [items]);
+
+  // const deleteTask = (taskId: string) => {
+  //   setItems((prev) => prev.filter((t) => t.id !== taskId));
+  // };
+
+  const categories = ["Main Course", "Drink", "Dessert"];
+  let sum = 0;
+  items.map((e) => {sum += Number(e.price)})
+  let sumMain = 0;
+  items.map((e) => {if(e.category === categories[0]) sumMain += Number(e.price)});
+  let sumdrink = 0;
+  items.map((e) => {if(e.category === categories[0]) sumMain += Number(e.price)});
+  let sumdress = 0;
+  
   return (
     <Container style={{ maxWidth: 600, margin: "auto", padding: 20 }}>
       <Title order={2} mb="md">
         Food Tracker
       </Title>
-      <Button>Add Food Item</Button>
+      {/* <Button onClick={() => setOpened(true)}>Add Food Item</Button> */}
+      {/* <AddTaskModal
+        opened={opened}
+        onClose={() => setOpened(false)}
+      /> */}
       {/* Type additional AddFoodModal here. */}
 
       <Divider my="md" />
       {/* Type additional total cost here. */}
-      <Title order={4}>Total cost: {} Baht</Title>
+      <Title order={4}>Total: {sum} Baht</Title>
       <Stack my="sm">{/* Type additional text here. */}</Stack>
-
+      <Text>
+      Main Course: {sumMain} Bath
+      </Text>
+      <Text>
+      Drink: {sumdrink} Bath
+      </Text>
+      <Text>
+      Dessert: {sumdress} Bath
+      </Text>
       <Divider my="md" />
       {/* Type additional card here. */}
 
-      <Stack>{/* Type additional food card list here. */}</Stack>
+      
     </Container>
   );
 }
