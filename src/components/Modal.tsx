4eca246b0e71807637492bf700829357a374d970
Ahiwamu <niceziio08@gmail.com>
2025-09-08 11:35:56 +0700

    test

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -19,19 +19,70 @@ type AddFoodModalProps = {
   ) => void;
 };
 
-export default function AddFoodModal({}: AddFoodModalProps) {
+export default function AddFoodModal({
+  opened,
+  onClose,
+  onAdd,
+}: AddFoodModalProps) {
   const [name, setName] = useState<string>("");
   const [price, setPrice] = useState<number | string>(0);
   const [quantity, setQuantity] = useState<number | string>(0);
   const [category, setCategory] = useState<string | null>(null);
 
-  const handleSubmit = () => {};
+  const handleSubmit = () => {
+    if (!name.trim() || !price || !quantity || !category) return;
+    onAdd(name, price, quantity, category);
+    setName("");
+    setPrice("");
+    setQuantity("");
+    setCategory("");
+    onClose();
+  };
 
   // หากต้องการแปง type string เป็น type number สามารถดูตัวอย่างนี้ได้
   let val_number: number = Number("500.0");
   console.log(val_number + 100); // 600.0
 
-  return {
-    /* Type additional text here. */
-  };
+  return (
+    <Modal opened={opened} onClose={onClose} title="Add an item">
+      <Stack>
+        <TextInput
+          label="Name of item"
+          description="Name of item"
+          error={!name.trim() && "Name of item is required"}
+          placeholder="e.g., Chicken rice"
+          value={name}
+          onChange={(e) => setName(e.currentTarget.value)}
+        />
+        <NumberInput
+          label="Price per dish"
+          description="Price per dish"
+          error={!price && "Price per dish is required"}
+          placeholder="0"
+          value={price}
+          onChange={setPrice}
+        />
+        <NumberInput
+          label="Quantity"
+          description="Quantity"
+          error={!quantity && "Quantity is required"}
+          placeholder="0"
+          value={quantity}
+          onChange={setQuantity}
+        />
+        <Select
+          label="Category"
+          description="Category"
+          error={!category && "Category is required"}
+          placeholder="Select a category"
+          data={['Main Course', 'Drink', 'Dessert']}
+          value={category} 
+          onChange={setCategory}
+        />
+        
+        <Button onClick={handleSubmit}>Summit</Button>
+
+      </Stack>
+    </Modal>
+  );
 }
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { AppShell } from "@mantine/core";
 import HeaderComponent from "../components/Header";
 import { useDisclosure } from "@mantine/hooks";
+import FooterComponent from "../components/Footer";
 
 export default function MainLayout() {
   const [opened, { toggle }] = useDisclosure();
@@ -18,6 +19,14 @@ export default function MainLayout() {
       <AppShell.Main>
         <Outlet />
       </AppShell.Main>
+      <AppShell.Footer>
+        <FooterComponent
+          courseName = "CPE-207"
+          year = "2025"
+          fullName = "Weeraphat Bunju"
+          studentId = "670610734"
+        />
+      </AppShell.Footer>
     </AppShell>
   );
 }
